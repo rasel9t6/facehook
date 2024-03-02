fd82941206d39fa826549c75a3d863177ae618b5
Rasel Hossen <raselhossen052@gmail.com>
2024-03-02 10:46:31 +0600

    setup-and-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-
 import './App.css';
 import HomePage from './pages/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-
-
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        />
+        <Route
+          path='/me'
+          element={<ProfilePage />}
+        />
+        <Route
+          path='/registration'
+          element={<RegistrationPage />}
+        />
+        <Route
+          path='*'
+          element={<NotFoundPage />}
+        />
+      </Routes>
     </>
   );
 }
